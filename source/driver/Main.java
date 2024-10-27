3bf338f067adbb2f739a34f10e166a869fb8e775
Rowel Razonable <razonabler23@gmail.com>
2024-10-26 23:20:11 -0700

    move main 'the driver' to main package

--- source/driver/Main.java
+++ source/driver/Main.java
@@ -1,9 +1,13 @@
 package driver;
 
+import program.bank.BankProgram;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		// execute the program
+		BankProgram.run();
 
 	}
 
